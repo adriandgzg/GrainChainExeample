b21232aa76d650532b1ace60511af8789b617ff3
Adrian Dominguez <Adriandgzg@gmail.com>
2020-03-20 01:03:45 -0600

    Initial Commit

---
+++ GrainChainExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrainChainExample
+//
+//  Created by Adrian Dominguez Gómez on 20/03/20.
+//  Copyright © 2020 Adrian Dominguez Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
